fa0cd83c44cf9e025ad122632d8935714612e9a3
poseidonchik <muha.rox@gmail.com>
2022-05-22 21:19:41 +0300

    Добавлены тесты

---
+++ src/test/java/shopTests/ShopTests.java
@@ -0,0 +1,80 @@
+package shopTests;
+
+import com.codeborne.selenide.SelenideElement;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class ShopTests {
+    private String body;
+
+    @BeforeAll
+    static void prepare() {
+        RestAssured.baseURI = "http://demowebshop.tricentis.com/";
+    }
+
+    @Test
+    void checkWishListAPI() {
+        body = "addtocart_53.EnteredQuantity=1";
+        step("Добавляем продукт", () -> {
+            given()
+                    .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                    .body(body)
+                    .log()
+                    .all()
+                    .when()
+                    .post("addproducttocart/details/53/2")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(200)
+                    .body("updatetopwishlistsectionhtml", is("(1)"))
+                    .body("message", is("The product has been added to your <a href=\"/wishlist\">wishlist</a>"));
+        });
+    }
+
+    @Test
+    void checkUsersAddress() {
+        String loginPassword = "poseidon@sea.com";
+        SelenideElement address = $(".address-list");
+
+        step("Забираем куки и вставляем в браузер", () -> {
+            String authorizationCookie = given()
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", loginPassword)
+                    .formParam("Password", loginPassword)
+                    .log()
+                    .all()
+                    .when()
+                    .post("login")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(302)
+                    .extract()
+                    .cookie("NOPCOMMERCE.AUTH");
+
+            step("Открываем браузер с самой легковесной страницей сайта", () ->
+                    open("Themes/DefaultClean/Content/images/logo.png"));
+
+            step("Вставляем куки в браузер", () ->
+                    getWebDriver().manage().addCookie(
+                            new Cookie("NOPCOMMERCE.AUTH", authorizationCookie)));
+        });
+        step("Проверяем данные пользователя", () -> {
+            open("/customer/info");
+            $("#FirstName").shouldHave(text(loginPassword));
+            $("#LastName").shouldHave(text(loginPassword));
+            $("#Email").shouldHave(text(loginPassword));
+        });
+    }
+}
